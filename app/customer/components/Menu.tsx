93a010cfa6cf5c59c7c5794ca69b9dd5e3893865
¨kevin180317¨ <ortega1gv@gmail.com>
2025-11-28 16:36:45 -0800

    update min estimatedtime

--- app/customer/components/Menu.tsx
+++ app/customer/components/Menu.tsx
@@ -758,7 +758,7 @@ export default function MenuPage() {
     if (uniqueProducts > 4) estimatedTime += 8;
     else if (uniqueProducts > 2) estimatedTime += 5;
 
-    return Math.min(Math.max(estimatedTime, 10), 45);
+    return Math.min(Math.max(estimatedTime, 5), 45);
   };
 
   const getPopularItems = () => {
